dd19fb1482ff2dc30d5ef9c9d70f5bc304c976ee
Matheus E. Muller <hello@memuller.com>
2016-09-16 11:23:54 -0300

    Adjusts default page layout.

--- page.php
+++ page.php
@@ -12,34 +12,40 @@
 
  get_header(); ?>
 
- <?php get_template_part( 'template-parts/featured-image' ); ?>
 
  <div id="page" role="main">
 
- <?php do_action( 'foundationpress_before_content' ); ?>
- <?php while ( have_posts() ) : the_post(); ?>
-   <article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
-       <header>
-           <h1 class="entry-title"><?php the_title(); ?></h1>
-       </header>
-       <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-       <div class="entry-content">
-           <?php the_content(); ?>
-           <?php edit_post_link( __( 'Edit', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
-       </div>
-       <footer>
-           <?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
-           <p><?php the_tags(); ?></p>
-       </footer>
-       <?php do_action( 'foundationpress_page_before_comments' ); ?>
-       <?php comments_template(); ?>
-       <?php do_action( 'foundationpress_page_after_comments' ); ?>
-   </article>
- <?php endwhile;?>
+   <?php get_sidebar(); ?>
+   <?php do_action( 'foundationpress_before_content' ); ?>
+   <?php while ( have_posts() ) : the_post(); ?>
+   	<article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
+   		<header>
+   			<h1 class="entry-title"><?php the_title(); ?></h1>
+   			<?php #foundationpress_entry_meta(); ?>
+   		</header>
+   		<?php do_action( 'foundationpress_post_before_entry_content' ); ?>
+   		<div class="entry-content">
 
- <?php do_action( 'foundationpress_after_content' ); ?>
- <?php get_sidebar(); ?>
+   		<?php
+   			if ( has_post_thumbnail() ) :
+   				the_post_thumbnail();
+   			endif;
+   		?>
 
- </div>
+   		<?php the_content(); ?>
 
- <?php get_footer();
+   		<?php edit_post_link( __( 'Edit', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
+   		</div>
+   		<?php //the_post_navigation(); ?>
+
+   		<?php do_action( 'foundationpress_post_before_comments' ); ?>
+   		<?php //comments_template(); ?>
+   		<?php do_action( 'foundationpress_post_after_comments' ); ?>
+
+   	</article>
+   <?php endwhile;?>
+
+   <?php do_action( 'foundationpress_after_content' ); ?>
+
+   </div>
+   <?php get_footer();
